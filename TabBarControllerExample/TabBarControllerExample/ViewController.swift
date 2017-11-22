ca51146dd7e6586fbf3d4afe21301c9a984f70b3
Cory Steers <cory.steers.gmu9@statefarm.com>
2017-11-22 13:18:54 -0600

    add simple TabBarController example

---
+++ TabBarControllerExample/TabBarControllerExample/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  TabBarControllerExample
+//
+//  Created by Cory Steers on 11/22/17.
+//  Copyright © 2017 Cory Steers. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
