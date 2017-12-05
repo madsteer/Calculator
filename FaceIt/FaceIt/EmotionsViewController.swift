7eefbf10aac97a58559a3bf5decdae61c3b2ee69
Cory Steers <cory.steers.gmu9@statefarm.com>
2017-12-05 08:04:38 -0600

    holding place to change branches

--- FaceIt/FaceIt/EmotionsViewController.swift
+++ FaceIt/FaceIt/EmotionsViewController.swift
@@ -8,20 +8,38 @@
 
 import UIKit
 
-extension EmotionsViewController: UISplitViewControllerDelegate {
-    func splitViewController(_ splitViewController: UISplitViewController, collapseSecondary secondaryViewController: UIViewController, onto primaryViewController: UIViewController) -> Bool {
-        return collapseDetailViewController
-    }
-}
+//extension EmotionsViewController: UISplitViewController {
+//    func splitViewController(_ splitViewController: UISplitViewController, collapseSecondary secondaryViewController: UIViewController, onto primaryViewController: UIViewController) -> Bool {
+//        return collapseDetailViewController
+//    }
+//}
 
-class EmotionsViewController: VCLLoggingViewController {
+class EmotionsViewController: UITableViewController {
 
     fileprivate var collapseDetailViewController = true
+
+    private var emotionalFaces: [(name:String, expression: FacialExpression)]  = [
+        ("sad", FacialExpression(eyes: .closed, mouth: .frown)),
+        ("happy", FacialExpression(eyes: .open, mouth: .smile)),
+        ("worried", FacialExpression(eyes: .open, mouth: .smirk))
+    ]
+
+//    override func viewDidLoad() {
+//        super.viewDidLoad()
+//        splitViewController?.delegate = self
+//    }
     
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        splitViewController?.delegate = self
+    // MARK: UITableViewDataSource
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return emotionalFaces.count
     }
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "Emotion Cell", for: indexPath)
+        cell.textLabel?.text = emotionalFaces[indexPath.row].name
+        return cell
+    }
+    
     // MARK: - Navigation
 
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
@@ -33,28 +51,13 @@ class EmotionsViewController: VCLLoggingViewController {
             destinationViewController.navigationItem.leftItemsSupplementBackButton = true
         }
         
-        /*
-         instead of
-         if let faceViewController = destinationViewController as? FaceViewController {
-             if let identifier = segue.identifier {
-                     if let expression = emotionalFaces[identifier] {
-                         <code here>
-                     }
-             }
-         }
-         ... do the following
-         */
         if let faceViewController = destinationViewController as? FaceViewController,
-            let identifier = segue.identifier,
-            let expression = emotionalFaces[identifier] {
-            faceViewController.expression = expression
-            faceViewController.navigationItem.title = (sender as? UIButton)?.currentTitle
+            let cell = sender as? UITableViewCell,
+            let indexPath = tableView.indexPath(for: cell) {
+//            let identifier = segue.identifier,
+//            let expression = emotionalFaces[identifier] {
+            faceViewController.expression = emotionalFaces[indexPath.row].expression
+            faceViewController.navigationItem.title = emotionalFaces[indexPath.row].name
         }
     }
-
-    private let emotionalFaces: Dictionary<String, FacialExpression> = [
-        "sad": FacialExpression(eyes: .closed, mouth: .frown),
-        "happy": FacialExpression(eyes: .open, mouth: .smile),
-        "worried": FacialExpression(eyes: .open, mouth: .smirk)
-    ]
 }
--- FaceIt/FaceIt/FaceViewController.swift
+++ FaceIt/FaceIt/FaceViewController.swift
@@ -78,14 +78,15 @@ class FaceViewController: VCLLoggingViewController {
         }
     }
     
-    private func updateUI() {
+    func updateUI() {
         switch expression.eyes {
         case .open:
             faceView?.eyesOpen = true
         case .closed:
             faceView?.eyesOpen = false
         case .squinting:
-            faceView?.eyesOpen = false
+            break
+//            faceView?.eyesOpen = false
         }
         faceView?.mouthCurvature = mouthCurvatures[expression.mouth] ?? 0.0
     }
